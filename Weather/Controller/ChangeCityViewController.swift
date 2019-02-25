bc487b85b19e49b6ad0fc48d664a66a9e93367aa
Jose Arguinzzones <jose.arguinzzones@hotmail.com>
2019-02-25 14:42:12 -0500

    Creating Weather app to portfolio!

---
+++ Weather/Controller/ChangeCityViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeCityViewController.swift
+//  Weather
+//
+//  Created by Jose M Arguinzzones on 2019-02-25.
+//  Copyright © 2019 Jose M Arguinzzones. All rights reserved.
+//
+
+import Foundation
---
+++ Weather/Controller/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Jose M Arguinzzones on 2019-02-24.
+//  Copyright © 2019 Jose M Arguinzzones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
